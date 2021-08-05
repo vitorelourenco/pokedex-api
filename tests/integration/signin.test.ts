58aaac3b8e36448a2f6f931974efa2a04dc270bc
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-05 17:26:14 -0300

    Fix bugs in signin tests

--- tests/integration/signin.test.ts
+++ tests/integration/signin.test.ts
@@ -10,8 +10,8 @@ import * as database from "../utils/database";
 import { createAndSignIn } from "../utils/database";
 import toMatchSchema from "../schemas/toMatchSchema";
 import tokenSchema from "../schemas/tokenSchema";
-import jwt from 'jsonwebtoken';
-import { Session } from "inspector";
+import jwt from "jsonwebtoken";
+import Session from "../../src/entities/Session";
 
 expect.extend({ toMatchSchema });
 
@@ -31,40 +31,49 @@ const agent = supertest(app);
 
 describe("POST /sign-in", () => {
   it("should respond with a status 200", async () => {
-    const {response} = await createAndSignIn();
+    const { response } = await createAndSignIn();
     expect(response.status).toBe(200);
   });
 
   it("should respond with status 400 when the email is not valid", async () => {
     const email = "email";
     const password = "password";
-    const response = await agent.post("/sign-in").send({email,password});
+    const response = await agent.post("/sign-in").send({ email, password });
     expect(response.status).toBe(400);
   });
 
   it("should respond with a JWT {'token':'$TOKEN'}", async () => {
-    const {response} = await createAndSignIn();
-    expect(response.body).toMatchSchema({tokenSchema});
+    const { response } = await createAndSignIn();
+    expect(response.body).toMatchSchema(tokenSchema);
   });
 
-  it("should start a new session for the user", async () => {
-    const {response} = await createAndSignIn();
+  it("should start a new session", async () => {
+    const { response } = await createAndSignIn();
     const token = response.body.token;
     const key = process.env.JWT_SECRET;
-    const data = JSON.parse(jwt.verify(token, key).toString());
-    const sessionId = data?.sessionId;
-    const hasSession = !!getRepository(Session).findOne({where:{id:sessionId}})
+
+    let hasSession: boolean;
+    try {
+      const data = jwt.verify(token, key) as { sessionId: number };
+      const id = data.sessionId;
+      const session = await getRepository(Session).findOne({ where: { id } });
+      hasSession = !!session;
+    } catch (err) {
+      hasSession = false;
+    }
 
     expect(hasSession).toBe(true);
   });
 
   it("should reject invalid credentials with status 401", async () => {
-    const {email, password} = await createAndSignIn();
-    const newEmail = email;
-    const newPassword = password + "a"
-    const response = await agent.post("/sign-in").send({newEmail,newPassword});
-    
+    const newUser = await createUser({});
+    await newUser.saveToDatabase();
+    const { email, password } = newUser.reqData;
+    const wrongPassword = password + "a";
+    const response = await agent
+      .post("/sign-in")
+      .send({ email, password: wrongPassword });
+
     expect(response.status).toBe(401);
   });
 });
-
