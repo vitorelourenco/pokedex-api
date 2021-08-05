5742ce5c4cd5c753977836510cc50dcba30a246f
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-05 12:26:26 -0300

    Make route for user creation

---
+++ src/protocols/DeepValidationError.ts
@@ -0,0 +1,8 @@
+export class DeepValidationError{
+  code:number;
+  message: string;
+  constructor(code:number, message:string){
+    this.code = code;
+    this.message = message;
+  }
+}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,28 @@
-import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import bcrypt from "bcrypt";
 
 import User from "../entities/User";
+import UserCreate from "../protocols/UserCreate";
+import { DeepValidationError } from "../protocols/DeepValidationError";
 
-export async function getUsers () {
-  const users = await getRepository(User).find({
-    select: ["id", "email"]
-  });
-  
-  return users;
+const emails: { [key: string]: boolean } = {};
+
+export async function loadEmails() {
+  const userList = await getRepository(User).find({ select: ["email"] });
+  userList.forEach((user) => (emails[user.email] = true));
+}
+
+export async function create(user: UserCreate) {
+  if (emails[user.email])
+    throw new DeepValidationError(409, "email is already in use");
+
+  try {
+    emails[user.email] = true;
+    const hashedPassword = bcrypt.hashSync(user.password, 10);
+    const safeUser = getRepository(User).create({email:user.email, password:hashedPassword});
+    await getRepository(User).save(safeUser);
+  } catch (err){
+    delete emails[user.email];
+    throw err;
+  }
 }
