c9a148de0af0f87f2c887ed950948fce235f6663
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-05 17:24:21 -0300

    Change userSchema to userSchemas

--- src/schemas/userSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const userSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(1).required(),
-  confirmPassword: Joi.ref("password")
-});
-
-export default userSchema;
