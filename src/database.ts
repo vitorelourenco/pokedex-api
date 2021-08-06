b447919e40d6fc613511867f151b38529024c55e
Vitor Lourenco <vitor.registros@lourencos.net>
2021-08-06 16:14:27 -0300

    Revert "Remove ssl"
    This reverts commit c020c328aba067d331cf1b88ee74b3af19b570b8.

--- src/database.ts
+++ src/database.ts
@@ -11,6 +11,7 @@ export default async function connect () {
     type: "postgres",
     url: process.env.DATABASE_URL,
     entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
+    ssl: process.env.NODE_ENV === 'production',
     extra: {
       ssl: {
         rejectUnauthorized: false
